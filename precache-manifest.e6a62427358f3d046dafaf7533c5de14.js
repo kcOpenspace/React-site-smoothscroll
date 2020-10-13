6383c41016a58489c572f752b0a183e43660fdb9
unknown <ken_chan_86@yahoo.com.au>
2020-10-13 13:20:26 +0800

    Updates

---
+++ precache-manifest.e6a62427358f3d046dafaf7533c5de14.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a7570b4a4fb98117b9300e2388d2fa7",
+    "url": "/kcOpenspace/React-site-smoothscroll/index.html"
+  },
+  {
+    "revision": "9d4f05026d704aba643c",
+    "url": "/kcOpenspace/React-site-smoothscroll/static/css/main.5c603e17.chunk.css"
+  },
+  {
+    "revision": "925b8e7c1a108ef3bfa2",
+    "url": "/kcOpenspace/React-site-smoothscroll/static/js/2.de3f4248.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/kcOpenspace/React-site-smoothscroll/static/js/2.de3f4248.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d4f05026d704aba643c",
+    "url": "/kcOpenspace/React-site-smoothscroll/static/js/main.9beee88b.chunk.js"
+  },
+  {
+    "revision": "eeb5390b3488e6f4d83c",
+    "url": "/kcOpenspace/React-site-smoothscroll/static/js/runtime-main.260d47b3.js"
+  },
+  {
+    "revision": "fe5473345ee35e2e6631a850cd5d313d",
+    "url": "/kcOpenspace/React-site-smoothscroll/static/media/svg-1.fe547334.svg"
+  },
+  {
+    "revision": "c974ff5cf7b84feca69f3fd02929d6e5",
+    "url": "/kcOpenspace/React-site-smoothscroll/static/media/svg-2.c974ff5c.svg"
+  },
+  {
+    "revision": "785a8806aa44bc20d90ec6a00125d5ff",
+    "url": "/kcOpenspace/React-site-smoothscroll/static/media/svg-3.785a8806.svg"
+  },
+  {
+    "revision": "4a122f73bc839268339ab652e3ec374d",
+    "url": "/kcOpenspace/React-site-smoothscroll/static/media/svg-4.4a122f73.svg"
+  },
+  {
+    "revision": "3ea0931b653a6121a2df98b130a5acaf",
+    "url": "/kcOpenspace/React-site-smoothscroll/static/media/video.3ea0931b.mp4"
+  }
+]);
